880058affaa442e550de9bdb468f5ef776603bd0
folorunsoemmanuel01-boop <folorunsoemmanuel01@gmail.com>
2026-08-21 00:41:57 -0800

    Update intro.py

--- intro.py
+++ intro.py
@@ -3,8 +3,8 @@ import streamlit as st
 def main():
   st.title("Introduction to Streamlit")
 
-name = st.text_input("Enter your name:")
-st.write(f"Hello,{name}")
+  name = st.text_input("Enter your name:")
+  st.write(f"Hello,{name}")
 
 if __name__=="__main__":
   main()
