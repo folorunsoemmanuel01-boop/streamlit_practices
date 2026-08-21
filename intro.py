34352c93f8d162ad6ddeed1359b842ab7db703f8
folorunsoemmanuel01-boop <folorunsoemmanuel01@gmail.com>
2026-08-21 04:14:20 -0800

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,10 +1,33 @@
-import streamlit as st
+# import streamlit as st
 
-def main():
-  st.title("Introduction to Streamlit")
+# def main():
+#   st.title("Introduction to Streamlit")
 
-  name = st.text_input("Enter your name:")
-  st.write(f"Hello,{name}")
+#   name = st.text_input("Enter your name:")
+#   st.write(f"Hello,{name}")
 
-if __name__=="__main__":
-  main()
+# if __name__=="__main__":
+#   main()
+
+import random
+
+def hangman():
+  st.title("Hangman Game")
+  words = ["python","game"]
+  word = random.choice(words)
+
+  play = 0
+
+  while play < 3:
+    guess = input("Enter a character")
+    if guess.isalpha() and len(guess) == 1
+        st.write(guess)
+      if guess in word:
+        st.write(f"The position of the guess is {word.find{guess}}")
+        st.write(f"Yes, The character {guess} is in {word}") 
+      else:
+        st.write("No")
+    else:
+      st.write("Invalid character")
+
+hangman()
