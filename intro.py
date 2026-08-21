2ccb36df102bb8e14bd721c1a1b4846289797f03
folorunsoemmanuel01-boop <folorunsoemmanuel01@gmail.com>
2026-08-21 04:36:42 -0800

    Update intro.py

--- intro.py
+++ intro.py
@@ -9,25 +9,28 @@ import streamlit as st
 # if __name__=="__main__":
 #   main()
 
-import random
+# import random
 
-def hangman():
-  st.title("Hangman Game")
+# def hangman():
+#   st.title("Hangman Game")
   
-  words = ["python","game"]
-  word = random.choice(words)
+#   words = ["python","game"]
+#   word = random.choice(words)
 
-  guess = st.text_input("Enter a character")
+#   guess = st.text_input("Enter a character")
   
-  if guess.isalpha() and len(guess) == 1:
-      st.write(guess)
+#   if guess.isalpha() and len(guess) == 1:
+#       st.write(guess)
     
-      if guess in word:
-        st.write(f"The position of the guess is {word.find(guess)}")
-        st.write(f"Yes, The character {guess} is in {word}") 
-      else:
-        st.write("No")
-  else:
-      st.write("Invalid character")
+#       if guess in word:
+#         st.write(f"The position of the guess is {word.find(guess)}")
+#         st.write(f"Yes, The character {guess} is in {word}") 
+#       else:
+#         st.write("No")
+#   else:
+#       st.write("Invalid character")
 
-hangman()
+# hangman()
+
+for i in range(1,11)
+  st.write(f"2 * {i} = {2*i}")
