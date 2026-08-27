d42e16592e41713c7a4adbb7c62587801e1d42d0
folorunsoemmanuel01-boop <folorunsoemmanuel01@gmail.com>
2026-08-27 02:36:30 -0800

    Update intro.py

--- intro.py
+++ intro.py
@@ -89,6 +89,13 @@ def hangman():
 
 hangman()
 
+for num in range(1,13):
+   st.write(f"\nMultiplication Table for {number}
+
+   for multiplier in range(1,16):
+      st.write(f"{number} X {multiplier} = {number * multiplier}")
+   
+
 # for i in range(1,11):
 #    st.write(f"2 * {i} = {2*i}")
 
