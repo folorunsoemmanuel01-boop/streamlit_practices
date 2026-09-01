8928d02338aa4a36c710ae6fd750ce5a40b6c630
folorunsoemmanuel01-boop <folorunsoemmanuel01@gmail.com>
2026-09-01 02:50:32 -0800

    Update intro.py

--- intro.py
+++ intro.py
@@ -10,30 +10,34 @@ if __name__=="__main__":
    main()
 
 
+import streamlit as st
+
+
 def usersInput():
 
-   st.title("Enter Numbers")
+    st.title("Enter Numbers")
 
-   if "userList" not in st.session_state:
-      st.session_state.userList = []
-      
-      userInput = st.text_input("Enter a number:")
-      
-      if st.button("Add Number"):
-         
-          if userInput.isdigit():
-              st.session_state.userList.append(int(userInput))
-              st.success("Number aded!")
-             
-          elif userInput == "":
-              st.warning("Pls enter a number")
-            
-          else:
-              st.error("Invalid Character")
+    if "userList" not in st.session_state:
+        st.session_state.userList = []
 
-      if st.button("Done"):
-          st.write("Your Final List:")
-          st.write(st.session_state.userList)
+    userInput = st.text_input("Enter a number:")
+
+    if st.button("Add Number"):
+
+        if userInput.isdigit():
+            st.session_state.userList.append(int(userInput))
+            st.success("Number added!")
+
+        elif userInput == "":
+            st.warning("Please enter a number")
+
+        else:
+            st.error("Invalid Character")
+
+    if st.button("Done"):
+        st.write("Your Final List:")
+        st.write(st.session_state.userList)
+
 
 usersInput()
    
