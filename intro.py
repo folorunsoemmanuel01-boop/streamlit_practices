749382d1df965ee4d247cfe2b98a87baa52d854c
folorunsoemmanuel01-boop <folorunsoemmanuel01@gmail.com>
2026-09-01 06:07:38 -0800

    Update intro.py

--- intro.py
+++ intro.py
@@ -10,19 +10,6 @@ if __name__=="__main__":
    main()
 
 
-# STREAMLIT HANGMAN 
-import streamlit as st
-
-def main():
-   st.title("Introduction to Streamlit")
-
-   name = st.text_input("Enter your name:")
-   st.write(f"Hello,{name}")
-
-if __name__=="__main__":
-   main()
-
-
 import random
 import streamlit as st
 
