c9825f251ae2019f1a0049e82d90bd7107fb7465
folorunsoemmanuel01-boop <folorunsoemmanuel01@gmail.com>
2026-09-02 03:40:46 -0800

    Update try.py

--- try.py
+++ try.py
@@ -1,9 +1,40 @@
+# import streamlit as st
+
+# number = st.text_input("Enter num")
+
+# st.write(f"Multiplication table for {number}")
+
+# for num in range (1,16):
+#   st.write(f"{number} X {num} = {number * num}
+
+  
+
 import streamlit as st
+def usersInput():
 
-number = st.text_input("Enter num")
+    st.title("Enter Numbers")
 
-st.write(f"Multiplication table for {number}")
+    if "userList" not in st.session_state:
+        st.session_state["userList"] = []
 
-for num in range (1,16):
-  st.write(f"{number} X {num} = {number * num}
+    userInput = st.text_input("Enter a number:")
+
+    if st.button("Add Number"):
+
+        if userInput.isdigit():
+            st.session_state["userList"].append(int(userInput))
+            st.success("Number added!")
+
+        elif userInput == "":
+            st.warning("Please enter a number")
+
+        else:
+            st.error("Invalid Character")
+
+    if st.button("Done"):
+        st.write("Your Final List:")
+        st.write(st.session_state["userList"])
+
+
+usersInput()
   
