027f20a08d7fb319f478bc351d5d5319532ba134
folorunsoemmanuel01-boop <folorunsoemmanuel01@gmail.com>
2026-09-08 04:16:41 -0800

    Update hangman_2.py

--- hangman_2.py
+++ hangman_2.py
@@ -11,33 +11,34 @@ def main():
     st.session_state.attempts = 5
   
   user_guess = st.text_input("Enter a character:").lower()
+
+  if st.button(Play)
+    if len(user_guess) == 1:
+      if user_guess in st.session_state.choice:
+        pos = st.session_state.choice.find(user_guess)
+        st.session_state.guesses[pos] = user_guess
   
-  if len(user_guess) == 1:
-    if user_guess in st.session_state.choice:
-      pos = st.session_state.choice.find(user_guess)
-      st.session_state.guesses[pos] = user_guess
-
-      st.write(" ".join(st.session_state.guesses))
-      if list(st.session_state.choice) == st.session_state.guesses:
-        if st.success(f"Congratulations, you won the game with {5 - st.session_state.attempts} attempts left"):
-           
-          if st.button("Play Again!"):
-              for key in st.session_state.keys():
-                  del st.session_state[key]
-
-              st.rerun()
-    else:
-      st.session_state.attempts -= 1
-      if st.session_state.attempts==0:
-        st.error("Game Over!")
-        for key in st.session_state.keys():
-              del st.session_state[key]
-        st.rerun()
-      
+        st.write(" ".join(st.session_state.guesses))
+        if list(st.session_state.choice) == st.session_state.guesses:
+          if st.success(f"Congratulations, you won the game with {5 - st.session_state.attempts} attempts left"):
+             
+            if st.button("Play Again!"):
+                for key in st.session_state.keys():
+                    del st.session_state[key]
+  
+                st.rerun()
       else:
-        st.write(f"You have {st.session_state.attempts} attempts left.")
-  else:
-    st.write("This is not how to play the game!!")
+        st.session_state.attempts -= 1
+        if st.session_state.attempts==0:
+          st.error("Game Over!")
+          for key in st.session_state.keys():
+                del st.session_state[key]
+          st.rerun()
+        
+        else:
+          st.write(f"You have {st.session_state.attempts} attempts left.")
+    else:
+      st.write("This is not how to play the game!!")
 
  
 
